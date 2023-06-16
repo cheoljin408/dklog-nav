1f22b72a4457c6bc605f20e0e42d2196000b2dac
cheoljin408 <kcj409@naver.com>
2023-06-16 12:16:52 +0900

    first commit

---
+++ src/main/java/com/example/dklognav/DklogNavApplication.java
@@ -0,0 +1,13 @@
+package com.example.dklognav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DklogNavApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DklogNavApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dklognav/DklogNavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dklognav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DklogNavApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
